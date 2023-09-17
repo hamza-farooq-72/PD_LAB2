9e616341de1604adf0c0c8687787525566298953
Hamza farooq <144421730+hamza-farooq-72@users.noreply.github.com>
2023-09-17 02:43:49 -0700

    Add files via upload

---
+++ foodbowl.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+system("color 47");
+
+cout<<"               *       *      *                                 "<<endl;
+cout<<"                *       *      *                                 "<<endl;
+cout<<"                *        *      *                               "<<endl;
+cout<<"              *         *      *                             "<<endl;
+cout<<"             *        *      *                                "<<endl;
+cout<<"               *        *      *                                "<<endl;
+cout<<"                 *        *       *                              "<<endl;
+cout<<"       *****************************************                 "<<endl;
+cout<<"        *                                     *               "<<endl;
+cout<<"          *                                  *               "<<endl;
+cout<<"            *                               *                  "<<endl;
+cout<<"              *                           *                   "<<endl;
+cout<<"                *************************                     "<<endl;
+cout<<endl;
+cout<<"              #WELCOME TO FOODY APPLICATIONS                     "<<endl;
+return 0;
+}
