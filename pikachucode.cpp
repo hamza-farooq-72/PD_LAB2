095dc171c618b38bacfd3321826c95c3a7eee9c6
Hamza farooq <144421730+hamza-farooq-72@users.noreply.github.com>
2023-09-17 02:49:29 -0700

    Add files via upload

---
+++ pikachucode.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+system("color 06");
+
+cout<<"    ';--.          ---'          "<<endl;
+cout<<"      '. '!_...._/'.-''          "<<endl;
+cout<<"         !        /      '        "<<endl;
+cout<<"         /()   () !    .'  '-.- "<<endl;
+cout<<"        |)  .    ()!  /   -.' "<<endl;
+cout<<"         |  -'-     '; '. <   "<<endl;
+cout<<"         ;.__     ';|   > |   "<<endl;
+cout<<"        /  ,   / ,  |.-'.-'   "<<endl;
+cout<<"       (_/    (_/ ,;|.<'         "<<endl;
+cout<<"         !    ,     ;--'       "<<endl;
+cout<<"          >   !    /          "<<endl;
+cout<<"         (_,-''> .'          "<<endl;
+cout<<"           (_,'             "<<endl;
+
+
+}
