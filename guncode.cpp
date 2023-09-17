b41a61b1911db4c126c045d5f600e90d80d6e5ac
Hamza farooq <144421730+hamza-farooq-72@users.noreply.github.com>
2023-09-17 02:46:41 -0700

    Add files via upload

---
+++ guncode.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+system("color 03");
+
+
+cout <<"                         +--^----------,-------,-----,--------^-,                      "<<endl;                         
+cout <<"                          | |||||||||   '-------'     |          O                     "<<endl;     
+cout <<"                          '+--------------------------^----------|                     "<<endl;     
+cout <<"                            ',---------,--------,--------------'                        "<<endl;     
+cout <<"                              / XXXXXX /'((      /'                                     "<<endl;     
+cout <<"                             / XXXXXX /   (      /'                                      "<<endl;     
+cout <<"                            / XXXXXX /'------'                                         "<<endl;     
+cout <<"                           / XXXXXX /                                                  "<<endl;     
+cout <<"                          / XXXXXX /                                                   "<<endl;     
+cout <<"                         (________(                                                    "<<endl;     
+cout <<"                          '______'                                                     "<<endl;     
+
+}
